970c9563681530c70aefc4f0bb89fcc7b7e4ba08
ashish-greycube <mr.ashish.shah@gmail.com>
2021-10-21 20:24:02 +0530

    fix: donot create for template and added options value

--- bglstore/item_controller.py
+++ bglstore/item_controller.py
@@ -7,20 +7,21 @@ from barcode.writer import SVGWriter
 from frappe.model.naming import make_autoname
 
 def validate_and_create_barcode(self,method):
-		if not self.item_barcode_cf:
-			self.item_barcode_cf = make_autoname('BGLS.#',self.doctype)
-			self.append('barcodes',{'barcode':self.item_barcode_cf,'barcode_type':''})
+		if self.has_variants==0:
+			if not self.item_barcode_cf:
+				self.item_barcode_cf = make_autoname('BGLS.#',self.doctype)
+				self.append('barcodes',{'barcode':self.item_barcode_cf,'barcode_type':''})
 
-		if not self.item_barcode_image_cf:
-			buffer = io.BytesIO()
-			Code128(str(self.item_barcode_cf), writer=SVGWriter()).write(buffer)
-			_file = frappe.get_doc({
-				"doctype": "File",
-				"file_name": "%s.svg" % frappe.generate_hash()[:8],
-				"attached_to_doctype": self.doctype,
-				"attached_to_name": self.name,
-				"attached_to_field":'item_barcode_image_cf',
-				"content": buffer.getvalue()
-			})
-			_file.save()
-			self.item_barcode_image_cf= _file.file_url
+			if not self.item_barcode_image_cf:
+				buffer = io.BytesIO()
+				Code128(str(self.item_barcode_cf), writer=SVGWriter()).write(buffer)
+				_file = frappe.get_doc({
+					"doctype": "File",
+					"file_name": "%s.svg" % frappe.generate_hash()[:8],
+					"attached_to_doctype": self.doctype,
+					"attached_to_name": self.name,
+					"attached_to_field":'item_barcode_image_cf',
+					"content": buffer.getvalue()
+				})
+				_file.save()
+				self.item_barcode_image_cf= _file.file_url
