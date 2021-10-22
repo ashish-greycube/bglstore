a25e063b37c360a5a61aaacd64b9bbbe9b574a0b
ashish-greycube <mr.ashish.shah@gmail.com>
2021-10-22 09:54:00 +0530

    fix: image rendering of barcode

--- bglstore/item_controller.py
+++ bglstore/item_controller.py
@@ -8,20 +8,19 @@ from frappe.model.naming import make_autoname
 
 def validate_and_create_barcode(self,method):
 		if self.has_variants==0:
-			if not self.item_barcode_cf:
-				self.item_barcode_cf = make_autoname('BGLS.#',self.doctype)
-				self.append('barcodes',{'barcode':self.item_barcode_cf,'barcode_type':''})
+				if not self.item_barcode_image_cf:
+					item_barcode_cf = make_autoname('BGLS.#',self.doctype)
+					self.append('barcodes',{'barcode':item_barcode_cf,'barcode_type':''})
 
-			if not self.item_barcode_image_cf:
-				buffer = io.BytesIO()
-				Code128(str(self.item_barcode_cf), writer=SVGWriter()).write(buffer)
-				_file = frappe.get_doc({
-					"doctype": "File",
-					"file_name": "%s.svg" % frappe.generate_hash()[:8],
-					"attached_to_doctype": self.doctype,
-					"attached_to_name": self.name,
-					"attached_to_field":'item_barcode_image_cf',
-					"content": buffer.getvalue()
-				})
-				_file.save()
-				self.item_barcode_image_cf= _file.file_url
+					buffer = io.BytesIO()
+					Code128(str(item_barcode_cf), writer=SVGWriter()).write(buffer)
+					_file = frappe.get_doc({
+						"doctype": "File",
+						"file_name": "%s.svg" % frappe.generate_hash()[:8],
+						"attached_to_doctype": self.doctype,
+						"attached_to_name": self.name,
+						"attached_to_field":'item_barcode_image_cf',
+						"content": buffer.getvalue()
+					})
+					_file.save()
+					self.item_barcode_image_cf= _file.file_url
